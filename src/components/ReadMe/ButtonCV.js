ab9bdb2a6fbed3786efe8d62b15398a899a8bf24
sarastackhouse <sarapalacios7@gmail.com>
2021-04-23 23:44:45 +0200

    url cv changed

--- src/components/ReadMe/ButtonCV.js
+++ src/components/ReadMe/ButtonCV.js
@@ -12,7 +12,7 @@ const ButtonCV = () => {
           target="_blank"
           rel="noopener noreferrer"
           className="cta-btn btn-cv"
-          href="https://drive.google.com/file/d/1hn601KLMTsc_n74lVej0nA4HH9b4EA_N/view?usp=sharing"
+          href="https://drive.google.com/file/d/1d68pJMZHrHJF1wJX4ZijOSr8uaqtgKWZ/view?usp=sharing"
         >
           {i18n.t('resume')}
       </a>
@@ -21,7 +21,7 @@ const ButtonCV = () => {
           target="_blank"
           rel="noopener noreferrer"
           className="cta-btn btn-cv"
-          href="https://drive.google.com/file/d/1q9GUEOfXDvkGKXTiokyFeypANdSfAmsV/view?usp=sharing"
+          href="https://drive.google.com/file/d/15_8WiNxoGMlNUCUmS_jI9_pIvWkSoORK/view?usp=sharing"
         >
           {i18n.t('resume')}
       </a>
